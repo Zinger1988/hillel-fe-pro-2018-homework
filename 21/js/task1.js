3fa089a170b4fce3bd7810a1c7097105d4106ff5
Sergey <sergeygonchar88@gmail.com>
2018-05-28 01:15:56 +0300

    task 19 modify JS

--- 21/js/task1.js
+++ 21/js/task1.js
@@ -1,33 +1,16 @@
-// var targetArr = [3,2,3,4,5,"str", false, true],
-//     targetType = "boolean";
-
-// function* gen(arr, type) {
-//   var resArr = arr.filter( item => typeof item == type)
-//   yield resArr;
-// }
-
-// for (let item of gen(targetArr, targetType)){
-//   console.log(item)
-// }
-
-
 var list = [1, 2, 3]
 function square(x) { return x * x; }
 var squareGen = fmap(square, list);
 
 function fmap(fun, arr){
   
-  function* gen(fun, arr){
+  return (function* (fun, arr){
     for(let key of arr){
       yield fun(key);
     }
-  }
-
-  return gen(fun, arr)
+  })(fun, arr)
 
 }
-
-
 
 console.log(squareGen.next()); // 1
 console.log(squareGen.next()); // 4
